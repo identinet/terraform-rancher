639b8ed13d7012450cfd793b014cb13a1f3b62b9
Jan Christoph Ebersbach <jceb@e-jc.de>
2021-01-06 10:43:41 +0100

    fix: correct cluster variable name in outupt

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
 output "kube_config" {
-  value = rancher2_cluster.cluster.kube_config
+  value = rke_cluster.cluster.kube_config
 }
 
 output "node_command" {
-  value = rancher2_cluster.cluster.cluster_registration_token.0.node_command
+  value = rke_cluster.cluster.cluster_registration_token.0.node_command
 }
